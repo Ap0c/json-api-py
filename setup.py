d11c5cd36d1b763fb61e04af3144e10d69d789d5
Ap0c <Ap0c@users.noreply.github.com>
2015-12-21 18:19:36 +0000

    Added setup script.

---
+++ setup.py
@@ -0,0 +1,15 @@
+# ----- Imports ----- #
+
+from setuptools import setup
+
+
+# ----- Setup ----- #
+
+setup(name='json_api',
+	version='0.1.0',
+	description='''A simple, structured JSON API written in Python, for
+	communication between apps in a network.''',
+	author='Ap0c',
+	license='MIT',
+	packages=['json_api'],
+	zip_safe=False)
